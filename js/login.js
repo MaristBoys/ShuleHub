65bb2cd58899fbc47990aea4577789b4ec090fc4
MaristBoys <mwanzamaristschool@gmail.com>
2025-05-28 23:14:10 +0200

    index con login

---
+++ js/login.js
@@ -0,0 +1,24 @@
+function handleCredentialResponse(response) {
+  const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyS249iv_O3nnTjbHkHb6WNN7-ZmPegjMZUZbSYS4lgXEAWQQYNZzsNHGF9P2-cry_3Mw/exec";
+
+  const idToken = response.credential;
+
+  fetch(GOOGLE_SCRIPT_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ idToken: idToken })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      alert(`Benvenuto ${data.user.name} (${data.user.email})!`);
+      // Qui puoi caricare la tabella filtrata ecc.
+    } else {
+      alert('Accesso negato: ' + data.message);
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    alert('Errore di comunicazione');
+  });
+}
